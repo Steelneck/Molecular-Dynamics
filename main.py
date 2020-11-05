22ebb507d82355565f656ff3678b2ba109a992b3
Christoffer Lundgren <Christoffer.lundgren.92@outlook.com>
2020-11-05 16:54:04 +0100

    Updated the main.py so that is is actually a main function.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from ase.lattice.cubic import FaceCenteredCubic
 from calculations import Specific_Heat
 
-atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-                          symbol='Al',
-                          size=(1, 1, 1),
-                          pbc=True)
+def main():
+    atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+                              symbol='Al',
+                              size=(1, 1, 1),
+                              pbc=True)
+    Specific_Heat(atoms)
 
+    
 
-Specific_Heat(atoms)
+if __name__ == "__main__":
+    main()
+    
