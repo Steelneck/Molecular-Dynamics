0b013d8ccf6e40c96384eab5ce8e262323adf0e7
Steelneck <joesy466@student.liu.se>
2020-11-10 11:26:05 +0100

    Added directions for size i.e. x,y and z axis

--- init_values.py
+++ init_values.py
@@ -6,15 +6,18 @@ from ase.md.verlet import VelocityVerlet
 from ase import units
 from asap3 import EMT
 
+# Set variables
 Directions = [[1, 0, 0], [0, 1, 0], [0, 0, 1]] # Orientation of lattice (Miller indices basis vectors of supercell)
-Size = 10 # How many times fundamental repeat unit is repeated
+Size_X = 10 # How many times fundamental repeat unit is repeated
+Size_Y = 10
+Size_Z = 10
 Symbol = "Cu" # Element specified by atomic symbol e.g. Cu for copper
 Pbc = True # Set periodic boundary condition to True or False
 
 # Set up a crystal
 atoms = FaceCenteredCubic(directions=Directions,
                           symbol=Symbol,
-                          size=(Size, Size, Size),
+                          size=(Size_X, Size_Y, Size_Z),
                           pbc=Pbc)
 
 # Set the momenta corresponding to T=300K
@@ -22,3 +25,4 @@ MaxwellBoltzmannDistribution(atoms, 300 * units.kB)
 
 # Describe the interatomic interactions with the Effective Medium Theory
 atoms.calc = EMT()
+
