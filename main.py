11f069549ea2bb3bb79280328a6722a497d40065
Steelneck <joesy466@student.liu.se>
2020-11-12 05:36:24 +0100

    Quick fix to define printenergy in main for dyn.attach, will fix later

--- main.py
+++ main.py
@@ -6,14 +6,23 @@ def main():
     
     # Initiate the crystal based on the chosen variables
     # This will eventually become "Initiate the system" => system depends on user's choice
-    init()
+    atoms = init()
     
     # We want to run MD with constant energy using the VelocityVerlet algorithm.
     dyn = VelocityVerlet(atoms, 5 * units.fs)  # 5 fs time step.
+    
+    """ Keep this for now to see changes in energy in terminal when changing lattice.
+    This was a quick fix to solve the problem of attaching it to dyn """
+    def printenergy(a=atoms):  # store a reference to atoms in the definition.
+        """Function to print the potential, kinetic and total energy."""
+        epot = atoms.get_potential_energy() / len(atoms)
+        ekin = atoms.get_kinetic_energy() / len(atoms)
+        print('Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
+        'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin))
 
     # Now run the dynamics
     dyn.attach(printenergy, interval=3)
-    printenergy() # Use it for now to check results in terminal
+    printenergy # Use it for now to check results in terminal
     dyn.run(20)
 
 if __name__ == "__main__":
