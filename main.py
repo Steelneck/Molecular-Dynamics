608a1b8780124b2fb7f8c6bc7e3b896d16951723
Steelneck <joesy466@student.liu.se>
2020-11-12 05:00:19 +0100

    Added init-function that creates crystals for now

--- main.py
+++ main.py
@@ -3,22 +3,18 @@
 from init_values import *
 
 def main():
-
+    
+    # Initiate the crystal based on the chosen variables
+    # This will eventually become "Initiate the system" => system depends on user's choice
+    init()
+    
     # We want to run MD with constant energy using the VelocityVerlet algorithm.
     dyn = VelocityVerlet(atoms, 5 * units.fs)  # 5 fs time step.
 
-
-    def printenergy(a=atoms):  # store a reference to atoms in the definition.
-        """Function to print the potential, kinetic and total energy."""
-        epot = a.get_potential_energy() / len(a)
-        ekin = a.get_kinetic_energy() / len(a)
-        print('Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
-            'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin))
-
     # Now run the dynamics
-    dyn.attach(printenergy, interval=10)
-    printenergy()
-    dyn.run(200)
+    dyn.attach(printenergy, interval=3)
+    printenergy() # Use it for now to check results in terminal
+    dyn.run(20)
 
 if __name__ == "__main__":
     main()
