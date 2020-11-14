ef419294add0d723db355f43aa5e8d2f4b401f06
Mattis Ottosson <matot830@student.liu.se>
2020-11-14 18:43:37 +0100

    Changed files so that main.py runs the simulation and calls on calculation-functions in MSD.py.  Still need to find a good way of checking that the system is in equilibrium. Also changed the directory so it matches Christoffers hierarchy.

---
+++ Calculations/MSD.py
@@ -0,0 +1,45 @@
+"""Calculator for time averaged Mean Square Displacement"""
+"""Unclear if MSD should be calculated from t after equilibrium or from equilibrium to t=0"""
+
+from asap3 import Trajectory, FullNeighborList
+
+import numpy as np
+import tkinter
+
+def MSD_calc(a, t): 
+    """Function to calculate and print the time average of the mean square displacement (MSD) at time t."""
+    traj_MSD = Trajectory("atoms.traj")  #time evolution of trajectory
+    time = len(traj_MSD)-t 
+    pos_eq = traj_MSD[10].get_positions() #position of atoms when system has reached equilibrium
+    pos_t = traj_MSD[1].get_positions() #position of atoms at time t
+    diff = pos_t - pos_eq #displacement of all atoms from equilibrium to time t as a vector
+    diff_sq = np.absolute(diff)**2 
+    MSD = np.sum(diff_sq)/len(a) #Time averaged mean square displacement.
+    print("MSD = ", MSD)
+    return(MSD)
+
+def Self_diffuse(MSD, t):
+    """Function that  calculates the self-diffusion coefficient (D) at time t, based on the value of the mean square displacement."""
+    traj_MSD = Trajectory("atoms.traj")  
+    time = len(traj_MSD)-t
+    D = MSD/(6*time) #How to connect mean squre displacement to self-diffusion coefficient.
+    open("atoms.traj", "w").close()
+    print("D = ", D)
+    return(D)
+
+def Lindemann(a, MSD):
+     nblist = FullNeighborList(3.5, a).get_neighbors(1, -1) #Returns 3 lists containing information about nearest neighbors. 3rd list is the square of the distance to the neighbors.
+     d = np.sqrt(np.amin(nblist[2])) #distance to the nearest neighbor. Takes the minimum value of nblist.
+     L = MSD/d #Lindemann criterion. Expect melting when L>0.1
+     if L > 0.1:
+         #print("MELTING!")
+         print(L)
+         return True
+     else:
+         #print("Not melting.")
+         print(L)
+         return False
+
+
+
+
--- main.py
+++ main.py
@@ -1,49 +1,48 @@
-"""Demonstrates molecular dynamics with constant energy."""
-
 from ase.lattice.cubic import FaceCenteredCubic
+from ase.lattice.cubic import FaceCenteredCubic
+from ase.lattice.cubic import BodyCenteredCubic
+from ase.lattice.cubic import SimpleCubic
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md.verlet import VelocityVerlet
+from ase.lattice.bravais import Lattice
+from ase.atoms import Atoms
+from ase.md.langevin import Langevin
 from ase import units
+from ase.calculators.kim.kim import KIM
+
+from asap3 import EMT, Trajectory
+from asap3.io.trajectory import *
+
+from Calculations.MSD import MSD_calc
+from Calculations.MSD import Lindemann
+from Calculations.MSD import Self_diffuse
+
 
 def main():
-        
-    # Use Asap for a huge performance increase if it is installed
-    use_asap = True
-
-    if use_asap:
-        from asap3 import EMT
-        size = 10
-    else:
-        from ase.calculators.emt import EMT
-        size = 3
-        
-    # Set up a crystal
     atoms = FaceCenteredCubic(directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-                            symbol="Cu",
-                            size=(size, size, size),
-                            pbc=True)
+                          symbol="Cu",
+                          size=(1, 1, 1),
+                          pbc=True)
+    
 
     # Describe the interatomic interactions with the Effective Medium Theory
-    atoms.calc = EMT()
+    calc = KIM("LJ_ElliottAkerson_2015_Universal__MO_959249795837_003")
+    atoms.calc = calc
 
     # Set the momenta corresponding to T=300K
     MaxwellBoltzmannDistribution(atoms, 300 * units.kB)
 
     # We want to run MD with constant energy using the VelocityVerlet algorithm.
     dyn = VelocityVerlet(atoms, 5 * units.fs)  # 5 fs time step.
+    # dyn = Langevin(atoms, 5 * units.fs, units.kB * 300, 0.002)
 
-
-    def printenergy(a=atoms):  # store a reference to atoms in the definition.
-        """Function to print the potential, kinetic and total energy."""
-        epot = a.get_potential_energy() / len(a)
-        ekin = a.get_kinetic_energy() / len(a)
-        print('Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
-            'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin))
-
-    # Now run the dynamics
-    dyn.attach(printenergy, interval=10)
-    printenergy()
-    dyn.run(200)
+    traj = Trajectory("atoms.traj", "w", atoms)
+    dyn.attach(traj.write, interval=1)
+    dyn.run(20)
+    
+    MSD = MSD_calc(atoms, 10)
+    D = Self_diffuse(MSD, 10)
+    L = Lindemann(atoms, MSD)
 
 if __name__ == "__main__":
     main()
