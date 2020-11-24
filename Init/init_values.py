6dfc3441aebe44dd5cd7c38016b8ed8366adc0bb
Christoffer Lundgren <Christoffer.lundgren.92@outlook.com>
2020-11-24 16:33:54 +0100

    Having trouble getting information from initial_structure

--- Init/init_values.py
+++ Init/init_values.py
@@ -19,73 +19,93 @@ from ase.calculators.kim.kim import KIM
 from .init_functions import set_lattice
 from .init_functions import set_lattice_const
 
+# Pygen 
+from pymatgen.ext.matproj import MPRester
 
-""" This section is where the user changes values """
+m = MPRester('rXy9SNuvaCUyoVmTDjDT')
+data = m.query(criteria={"elements": ["Cu"]}, properties=["pretty_formula", "initial_structure"])
+# print(((data[0])["spacegroup"])["crystal_system"])
+print(type(data))
 
-# Set variables for your simulation
-# OBS! Combination of Directions and Miller only works when complete and consistent
-Directions = [[1, 0, 0], [0, 1, 0], [0, 0, 1]] # Orientation of lattice
-Miller = [None, None, None] # Basis of supercell and / or three surfaces
-Size_X = 10 # How many times fundamental repeat unit is repeated
-Size_Y = 10
-Size_Z = 10
-Symbol = "Cu" # Element specified by atomic symbol e.g. Cu for copper (OBS! requires string)
-Pbc = (True, True, True) # Set periodic boundary condition to True or False. 
-Bravais = FaceCenteredCubic # Set the lattice
-lc_a = 0 # When lattice constants are zero => FaceCenteredCubic retrieves lc_a from ase
-lc_b = 0
-lc_c = 0
-lc_alpha = 0 # Degrees
-lc_beta = 0
-lc_gamma = 0
-Temperature = 300
-Calculator = EMT()
-
-""" The following Bravais lattices can be used:
- SimpleCubic                 Lattice constant: a
- FaceCenteredCubic           Lattice constant: a (set a=0 to let ase set the constant)
- BodyCenteredCubic           Lattice constant: a
- SimpleTetragonal            Lattice constant: a,c
- CenteredTetragonal          Lattice constant: a,c
- SimpleOrthorhombic          Lattice constant: a,b,c
- BaseCenteredOrthorhombic    Lattice constant: a,b,c
- FaceCenteredOrthorhombic    Lattice constant: a,b,c
- BodyCenteredOrthorhombic    Lattice constant: a,b,c
- SimpleMonoclinic            Lattice constant: a,b,c,alpha
- BaseCenteredMonoclinic      Lattice constant: a,b,c,alpha
-"""
-
-""" The following Bravais lattices cannot be used:
- Diamond # (requires a basis - check ase how to define new lattice with Factory)
- Triclinic # Not sure how this one works, needs all constants a,b,c,alpha,beta,gamma
- Hexagonal # OBS! Currently broken, see ase/lattice/hexagonal.py line 60
- HexagonalClosedPacked # OBS! Currently broken (and requires a basis)
- Hexagonal / HexagonalClosedPacked needs constant a,c
- Graphite (requires a basis)
-"""
-
-""" The following Calculators can be used:
-EMT()
-    ASAP3 built in effective medium theory. Works for Ni, Cu, Pd, Ag, Pt and Au (and their alloys).
-Kim('Insert_openKIM_potential_here')
-    openKIM potentials can be found from https://openkim.org/
-"""
+data1 = (data[0])["initial_structure"]
 
 
 """ This section will initialize the system based on the variables """
+def init_MP():
+    i = 0
+    atoms_list = []
+    while i < len(data): 
+        # Set variables for your simulation
+        # OBS! Combination of Directions and Miller only works when complete and consistent
+        Directions = [[1, 0, 0], [0, 1, 0], [0, 0, 1]] # Orientation of lattice
+        Miller = [None, None, None] # Basis of supercell and / or three surfaces
+        Size_X = 10 # How many times fundamental repeat unit is repeated
+        Size_Y = 10
+        Size_Z = 10
+        Symbol = str((data[i])["pretty_formula"]) # Element specified by atomic symbol e.g. Cu for copper (OBS! requires string)
+        Pbc = (True, True, True) # Set periodic boundary condition to True or False. 
+        # if str(((data[i])["spacegroup"])["crystal_system"]) == "cubic":
+        #     Bravais = SimpleCubic # Set the lattice
+        # elif str(((data[i])["spacegroup"])["crystal_system"]) == "tetragonal":
+        #     Bravais = SimpleTetragonal # Set the lattice
+        # elif str(((data[i])["spacegroup"])["crystal_system"]) == "monoclinic":
+        #     Bravais = SimpleMonoclinic # Set the lattice
+        # else:
+        #     continue
+        Bravais = FaceCenteredCubic
+        lc_a = 0 # When lattice constants are zero => FaceCenteredCubic retrieves lc_a from ase
+        lc_b = 0
+        lc_c = 0
+        lc_alpha = 0 # Degrees
+        lc_beta = 0
+        lc_gamma = 0
+        Temperature = 300
+        Calculator = EMT()
+        i = i + 1
 
-def init():
+        """ This section is where the user changes values """
 
-    # Set the lattice constant
-    Lattice_Const = set_lattice_const(lc_a,
-                                      lc_b,
-                                      lc_c,
-                                      lc_alpha,
-                                      lc_beta,
-                                      lc_gamma)
-    
-    # Set up a crystals
-    atoms = set_lattice(Bravais,
+        """ The following Bravais lattices can be used:
+        SimpleCubic                 Lattice constant: a
+        FaceCenteredCubic           Lattice constant: a (set a=0 to let ase set the constant)
+        BodyCenteredCubic           Lattice constant: a
+        SimpleTetragonal            Lattice constant: a,c
+        CenteredTetragonal          Lattice constant: a,c
+        SimpleOrthorhombic          Lattice constant: a,b,c
+        BaseCenteredOrthorhombic    Lattice constant: a,b,c
+        FaceCenteredOrthorhombic    Lattice constant: a,b,c
+        BodyCenteredOrthorhombic    Lattice constant: a,b,c
+        SimpleMonoclinic            Lattice constant: a,b,c,alpha
+        BaseCenteredMonoclinic      Lattice constant: a,b,c,alpha
+        """
+
+        """ The following Bravais lattices cannot be used:
+        Diamond # (requires a basis - check ase how to define new lattice with Factory)
+        Triclinic # Not sure how this one works, needs all constants a,b,c,alpha,beta,gamma
+        Hexagonal # OBS! Currently broken, see ase/lattice/hexagonal.py line 60
+        HexagonalClosedPacked # OBS! Currently broken (and requires a basis)
+        Hexagonal / HexagonalClosedPacked needs constant a,c
+        Graphite (requires a basis)
+        """
+
+        """ The following Calculators can be used:
+        EMT()
+            ASAP3 built in effective medium theory. Works for Ni, Cu, Pd, Ag, Pt and Au (and their alloys).
+        KIM('Insert_openKIM_potential_here')
+            openKIM potentials can be found from https://openkim.org/
+            For standard Lennard-Jones potential use : LJ_ElliottAkerson_2015_Universal__MO_959249795837_003
+        """
+
+        # Set the lattice constant
+        Lattice_Const = set_lattice_const(lc_a,
+                                        lc_b,
+                                        lc_c,
+                                        lc_alpha,
+                                        lc_beta,
+                                        lc_gamma)
+
+        # Set up a crystals
+        atoms = set_lattice(Bravais,
                         Lattice_Const,
                         Directions,
                         Miller,
@@ -94,14 +114,17 @@ def init():
                         Size_Z,
                         Symbol,
                         Pbc) 
+
+        # Set the momenta corresponding to T=300K 
+        # (Note: Create a higher order function)
+        MaxwellBoltzmannDistribution(atoms, Temperature * units.kB)
+
+        # Describe the interatomic interactions with the Effective Medium Theory
+        # (Note: Create a higher ordet function)
+        atoms.calc = Calculator
+
+        atoms_list.append(atoms)
     
-    # Set the momenta corresponding to T=300K 
-    # (Note: Create a higher order function)
-    MaxwellBoltzmannDistribution(atoms, Temperature * units.kB)
-    
-    # Describe the interatomic interactions with the Effective Medium Theory
-    # (Note: Create a higher ordet function)
-    atoms.calc = Calculator
-    
-    return atoms
+    return atoms_list
+
 
--- main.py
+++ main.py
@@ -4,39 +4,34 @@ from Init.init_values import *
 import os
 import Calculations.calculations as calc
 from asap3 import Trajectory
-from pymatgen.io.cif import CifFile
-from pymatgen.io.ase import AseAtomsAdaptor
-from pymatgen.core.structure import IStructure
-from pymatgen.ext.matproj import MPRester
-
 
 def main():
-    with MPRester('rXy9SNuvaCUyoVmTDjDT') as m:
-        data = m.query(criteria={"elements": {"$all": ["Fe", "O"]}}, properties=["exp.tags", "icsd_ids"]) #Follows the quires for mongoDB. Gets all the data for every element that starts with
-        print(type(entries))
 
-    # # Initiate the crystal based on the chosen variables
-    # # This will eventually become "Initiate the system" => system depends on user's choice
-    # atoms = init()
+    # Initiate the crystal based on the chosen variables
+    # This will eventually become "Initiate the system" => system depends on user's choice
+    atoms = init_MP()
     
-    # # We want to run MD with constant energy using the VelocityVerlet algorithm.
-    # dyn = VelocityVerlet(atoms, 5 * units.fs)  # 5 fs time step.
+    
+    # We want to run MD with constant energy using the VelocityVerlet algorithm.
+    for atomobj in atoms:
+        
+        dyn = VelocityVerlet(atomobj, 5 * units.fs)  # 5 fs time step.
 
-    # traj = Trajectory("atoms.traj", "w", atoms)
-    
-    # dyn.attach(traj.write, interval=10)
-    # dyn.run(200)
-    
-    # calc.eq_traj(atoms) #Creates new .traj-file containing trajectory post equilibrium.
-    # if os.path.getsize("atoms_eq.traj") != 0:
-    #     traj_eq = Trajectory("atoms_eq.traj")
-    #     #If-statement that checks if we ever reached equilibrium.
-    #     MSD = calc.MSD_calc(atoms, 10)
-    #     D = calc.Self_diffuse(MSD, 10)
-    #     L = calc.Lindemann(atoms, MSD)
-    #     SHC = calc.Specific_Heat(atoms)
-    # else:
-    #     print("Something went wront, your system never reached equilibrium. No calculations are possible.")
+        traj = Trajectory("atoms.traj", "w", atomobj)
+        
+        dyn.attach(traj.write, interval=10)
+        dyn.run(200)
+        
+        calc.eq_traj(atomobj) #Creates new .traj-file containing trajectory post equilibrium.
+        if os.path.getsize("atoms_eq.traj") != 0:
+            traj_eq = Trajectory("atoms_eq.traj")
+            #If-statement that checks if we ever reached equilibrium.
+            MSD = calc.MSD_calc(atomobj, 10)
+            D = calc.Self_diffuse(MSD, 10)
+            L = calc.Lindemann(atomobj, MSD)
+            SHC = calc.Specific_Heat(atomobj)
+        else:
+            print("Something went wront, your system never reached equilibrium. No calculations are possible.")
 
 if __name__ == "__main__":
     main()
