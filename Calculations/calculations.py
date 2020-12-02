e83d968df63deaa98fee280ccc63c798c49eb504
Oskar Andersson <oskan803@student.liu.se>
2020-12-02 13:59:13 +0100

    Finalized lattice constant calculations, modified from ASE example.

--- Calculations/calculations.py
+++ Calculations/calculations.py
@@ -167,39 +167,38 @@ def calc_internal_temperature(myAtoms, trajectoryFileName, timeStepIndex):
     print("Average internal temperature:", avgTemperature)  
     return(avgTemperature)
 
-def calc_lattice_constant(atoms, atomName, targetCrystalName = None):
-    """ Calculates the lattice constants. IMPORTANT!: Only works for FCC crystals for now. """
+def calc_lattice_constant_fcc_cubic(atoms, atomName):
+    """ Calculates the lattice constants. IMPORTANT!: Only works for FCC cubic crystals. Calculates both a and c constant but those are equal for fcc cubic"""
     # Make a good initial guess on the lattice constant
     a0 = 3.52 / np.sqrt(2) 
     c0 = np.sqrt(8 / 3.0) * a0
     
-    traj = Trajectory('lattice.traj', 'w')                          # Create a traj file to store the results from calculations
+    fileName = "lattice_" + atomName + ".traj"                              # Create filename from atomname
+    traj = Trajectory(fileName, 'w')                                        # Create a traj file to store the results from calculations
 
     # Generate 9 calculations of potential energy for different a and c values. 
-    eps = 0.01
-    print(bulk('Ni', 'hcp', a=a0, c=c0).cell)
-    print(a0 * np.linspace(1 - eps, 1 + eps, 3))
-    print(c0 * np.linspace(1 - eps, 1 + eps, 3))
+    eps = 0.01                                                              # A small deviation to generate a few more constants
     for a in a0 * np.linspace(1 - eps, 1 + eps, 3):
         for c in c0 * np.linspace(1 - eps, 1 + eps, 3):
-            ni = bulk('Ni', 'hcp', a=a, c=c)
-            ni.calc = EMT()
-            ni.get_potential_energy()
-            traj.write(ni)
+            at = bulk(atomName, 'fcc', a=a, c=c, cubic=True)                # Use bulk to build a cell. Only config is cubic fcc.
+            #at.calc = atoms.calc                                           # Something doesn't work with copying calculator from atoms object. 
+            at.calc = EMT()                                                 # Assign a new calculator.
+            at.get_potential_energy()                                       
+            traj.write(at)                                                  # Write both bulk config and energy to trajectory file
 
     # Now we can get the energies and lattice constants from the traj file
-    configs = read('lattice.traj@:')
+    configs = read(fileName + "@:")
     energies = [config.get_potential_energy() for config in configs]        # Get the atoms objects from traj file. 
-    print(configs[0].cell)
+
     # From the bulk builder we can do at.cell to get the constant: a at position 0,0 and constant: c at position 2,2
-    a = np.array([config.cell[0, 0] for config in configs])         
+    a = np.array([config.cell[0, 0] for config in configs])                 
     c = np.array([config.cell[2, 2] for config in configs])
-    print("a ; c ", a, " ; ", c, "\n")
 
     # Fit the energy to lattice constants to the expression: 𝑝0+𝑝1𝑎+𝑝2𝑐+𝑝3𝑎^2+𝑝4𝑎𝑐+𝑝5𝑐^2
     functions = np.array([a**0, a, c, a**2, a * c, c**2])
     p = np.linalg.lstsq(functions.T, energies, rcond=-1)[0]
-    print("Polynomial:", p, "\n")
+    #print("Polynomial:", p, "\n")
+    #print("Functions: \n", functions.T)
     
     # Solve fitted function for a and c. The minimum is found by
     p0 = p[0]
@@ -207,42 +206,6 @@ def calc_lattice_constant(atoms, atomName, targetCrystalName = None):
     p2 = np.array([(2 * p[3], p[4]), (p[4], 2 * p[5])])
     a0, c0 = np.linalg.solve(p2.T, -p1)
 
-    print("Lattice constants a:", a0, "| c:", c0, "\n")
-
-    """
-    # Make a good initial guess on the lattice constant
-    a0 = 3.52 / np.sqrt(2) 
-    c0 = np.sqrt(8 / 3.0) * a0
-    
-    traj = Trajectory('lattice.traj', 'w')                          # Create a traj file to store the results from calculations
-
-    # Generate 9 calculations of potential energy for different a and c values. 
-    eps = 0.01                                                      # A small deviation to generate a few more constants
-    for a in a0 * np.linspace(1 - eps, 1 + eps, 3):
-        for c in c0 * np.linspace(1 - eps, 1 + eps, 3):
-            at = bulk(atomName, targetCrystalName, a=a, c=c)        # Use bulk to build a cell. 
-            at.calc = atoms.calc                                    # Set calculator to the same from our configurations.
-            at.get_potential_energy()                               # Get the potential energy from the generated cell.
-            traj.write(at)                                          # Write results to traj file.
-
-    # Now we can get the energies and lattice constants from the traj file
-    configs = read('lattice.traj@:')
-    energies = [config.get_potential_energy() for config in configs]        # Get the atoms objects from traj file. 
-    # From the bulk builder we can do at.cell to get the constant: a at position 0,0 and constant: c at position 2,2
-    a = np.array([config.cell[0, 0] for config in configs])         
-    c = np.array([config.cell[2, 2] for config in configs])
-
-    # Fit the energy to lattice constants to the expression: 𝑝0+𝑝1𝑎+𝑝2𝑐+𝑝3𝑎^2+𝑝4𝑎𝑐+𝑝5𝑐^2
-    functions = np.array([a**0, a, c, a**2, a * c, c**2])
-    p = np.linalg.lstsq(functions.T, energies, rcond=-1)[0]
-    
-    # Solve fitted function for a and c. The minimum is found by
-    p0 = p[0]
-    p1 = p[1:3]
-    p2 = np.array([(2 * p[3], p[4]),
-                (p[4], 2 * p[5])])
-    a0, c0 = np.linalg.solve(p2.T, -p1)
-
-    print("Lattice constants a:", a0, "| c:", c0)
-
-    """
+    print("Lattice constant a:", a0) 
+    return(a0)
+    #print("Lattice constants a:", a0, "| c:", c0, "\n") Uncomment if we want to print c also
--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def main():
     traj = Trajectory("atoms.traj")
     traj_eq = Trajectory("atoms_eq.traj", "w", atoms)
 
-    calc.calc_lattice_constant(atoms, Symbol)
+    calc.calc_lattice_constant_fcc_cubic(atoms, Symbol)
+    print(atoms.cell.lengths())
     
     calc.eq_traj(atoms, traj, traj_eq, Size_X * Size_Y * Size_Z)#Creates new .traj-file containing trajectory post equilibrium.
     if os.path.getsize("atoms_eq.traj") != 0: #If-statement that checks if we ever reached equilibrium. Returns a message if the traj-file is empty, otherwise does calculations.
