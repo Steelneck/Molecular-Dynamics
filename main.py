f1de7237d08f45b8f635c329cd2ada8ebcd85d3b
Steelneck <joesy466@student.liu.se>
2020-12-09 14:02:28 +0100

    Changing simulation to super_simulation. This branch will be for NSC computer only

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 """Demonstrates molecular dynamics with constant energy."""
 
-from Init.init_simulation import *
+from Init.init_super_simulation import *
 
 def main():  
     
-    simulation(EMT(),'Cu')
-    simulation(EMT(), "Ni")
-    simulation(EMT(), "Pd")
+    super_simulation(EMT(),'Cu')
     
 if __name__ == "__main__":
     main()
