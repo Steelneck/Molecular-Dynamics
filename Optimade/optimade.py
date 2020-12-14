09309ff95ea892983d7a0e2253c568ddf557389d
Christoffer Lundgren <Christoffer.lundgren.92@outlook.com>
2020-12-14 23:39:39 +0100

    Added a new file and folder to solve optimade translation

---
+++ Optimade/optimade.py
@@ -0,0 +1,19 @@
+def translate_to_optimade(atomobj):
+    Id = "blablabla" #Hitta på att få unika ID men samma för varje sample. Troligen en egen funktion
+    elements = 
+    nelements =
+    element_rations =
+    Chemical_formula_anonymous =
+    dimension_types = [1,1,1]
+    nperiodic_dimensions = [1,1,1]
+    lattice_vectrs =
+    cartesian_site_positions =
+    nsites =
+    species_at_sites =
+    species =
+    structure_features = [] #oklart! 
+
+
+
+
+    return None
