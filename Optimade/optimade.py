6d6875ff1f18f2b2a448dd8c7c0a7fef0803e0aa
Oskar Andersson <oskan803@student.liu.se>
2020-12-15 21:47:09 +0100

    Added some keys and values, more to go. Also added the json file to git for team members to view.

--- Optimade/optimade.py
+++ Optimade/optimade.py
@@ -1,6 +1,7 @@
 import json
 import hashlib
 from ase.atoms import *
+from ase.formula import Formula
 
 """
 This is the interface to convert the simulation output to optimade format.
@@ -19,27 +20,48 @@ species
 
 def translate_to_optimade(atomobj,MSD):
 
+    # Extract all data from atoms object
     id = 0                                                  # Init an id, changes below.
+    nsites = len(atomobj)
     """
-    #elements = 
-    #nelements = 
-    # element_ratios = 
+    One method if formula is depricated as stated in ase documentation, however the method below is cleaner.
+    Also makes 'elements' sync with 'element_ratios'. 
+    elements = []
+    for atomSymbol in atomobj.get_chemical_symbols():
+        if atomSymbol not in elements:
+            elements.append(atomSymbol)
+    """
+    symbols_count_dict = atomobj.symbols.formula.count()
+    elements = list(symbols_count_dict.keys())          # All elements as Chemical symbols in a list (only type)
+    element_ratios = []
+    for amount in symbols_count_dict.values():
+        element_ratios.append(float(amount/nsites))     # Ratio of each element in structure. Float-cast to ensure floats
+    
+    ###!!! Verkar inte vara en grej i dokumentationen men den är i exemplet....
+    # chemsys = "".join(elements)                         # All elements in a string
+    nelements = len(elements)                           # Number of _different_ elements
+   
     # Chemical_formula_anonymous =
     dimension_types = [1,1,1]
     nperiodic_dimensions = 3
     # lattice_vectrs =
     cartesian_site_positions = atomobj.get_positions()
-    nsites = len(atomobj)
     species_at_sites = atomobj.get_chemical_symbols()
     # species =
     # structure_features = [] #oklart! 
     group1_Mean_Self_cofficient = MSD #Detta ska in via prefix kvantiter. Hur man gör detta har jag ingen aning om. 
-    """ 
+     
     # Init a dictionary
     data_dict = {}
-
     # --- Add data with all required keys
     data_dict["_id"] = id
+    data_dict["elements"] = elements
+    data_dict["nelements"] = nelements
+    data_dict["nsites"] = nsites
+    data_dict["elements"] = elements
+    data_dict["element_ratios"] = element_ratios
+    
+    #print(element_ratios)
 
     # Make JSON object
     data_json = json.dumps(data_dict)
@@ -50,9 +72,9 @@ def translate_to_optimade(atomobj,MSD):
     # Add id to data object and recreate a json object to write to destinationfile
     data_dict["_id"] = {"oid" : unique_id}
     with open("Optimade/data_format_optimade.json", 'w') as json_data_file:
-        json.dump(data_dict, json_data_file)
+        json.dump(data_dict, json_data_file, indent=2)
 
-    return None
+    print("Data for optimade is temporarly stored in 'Optimade/data_format_optimade.json'.")
 
 
 """
